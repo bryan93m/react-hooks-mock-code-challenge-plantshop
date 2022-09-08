3511ed8225b7fedce8be5549335b5630a3d65958
Bryan Meadows <bryan1993meadows@gmail.com>
2022-09-08 10:14:56 -0600

    done

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
 
-function PlantCard() {
+function PlantCard({plant, deletePlant, updatePlant}) {
+  const [isStocked, setIsStocked] = useState(true)
+  const [priceInput, setPriceInput] = useState(plant.price)
+
+  function handleStock() {
+    setIsStocked(!isStocked)
+  }
+  const handleDelete = () => {
+    deletePlant(plant.id)
+  }
+
+
+  function handleSubmit(e){
+    e.preventDefault()
+    updatePlant(plant.id, {price: parseFloat(priceInput)})
+  }
+  
+
   return (
     <li className="card">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      <img src={plant.image} alt={plant.name} />
+      <h4>{plant.name}</h4>
+      <p>
+        Price: 
+        <form onSubmit={handleSubmit}>
+          <input 
+            name="price" 
+            type="text" 
+            value={priceInput} 
+            onChange={(e) => setPriceInput(e.target.value)}
+          />
+          <button type="submit">Update Price</button>
+        </form>
+      </p>
+      <div className="row">
+      {isStocked ? (
+        <button onClick={handleStock} className="primary">In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={handleStock}>Out of Stock</button>
       )}
+        <button onClick={handleDelete}>&times;</button>
+      </div>
     </li>
   );
 }
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,52 @@
-import React from "react";
-import NewPlantForm from "./NewPlantForm";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import NewPlantForm from "./NewPlantForm"
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([])
+  const [search, setSearch] = useState("")
+
+  useEffect(() => {
+    axios.get("http://localhost:6001/plants")
+    .then(res => {
+      setPlants(res.data)
+    })
+  }, [])
+
+  const createPlant = (newPlant) => {
+    axios.post("http://localhost:6001/plants", newPlant)
+    .then(res => {
+      setPlants([...plants, res.data])
+    })
+  }
+
+  const handleDelete = (id) => {
+    axios.delete(`http://localhost:6001/plants/${id}`)
+    .then(res => {
+      setPlants(plants.filter(plant => plant.id !== id))
+    })
+  }
+
+  const handleUpdate = (id, updatedPlant) => {
+    axios.patch(`http://localhost:6001/plants/${id}`, updatedPlant)
+    .then(res => {
+      setPlants(plants.map(plant => plant.id === id ? res.data : plant))
+    })
+  }
+
+
+
+  const displayedPlants = plants.filter(plant => plant.name.toLowerCase().includes(search.toLowerCase()))
+
+
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm createPlant={createPlant}/>
+      <Search onSearch={setSearch} />
+      <PlantList plants={displayedPlants} deletePlant={handleDelete} updatePlant={handleUpdate}/>
     </main>
   );
 }
